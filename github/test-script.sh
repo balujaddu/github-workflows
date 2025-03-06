6ad1fcf2057fc47c21e4d29c7981588648c066c7
balujaddu <96193971+balujaddu@users.noreply.github.com>
2025-03-06 23:48:37 +0530

    Update test-script.sh

--- github/test-script.sh
+++ github/test-script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-git remote add origin https://github.com/balujaddu/github-workflows.git
+git remote add origin https://github.com/balujaddu/devops-notes-app.git
 git pull origin master:master
 echo "Cloning repo completed"
 
-git checkout -b test-abc && git push origin test-abc
-git checkout -b test-xyz && git push origin test-xyz
-echo "Created new branches"
-pwd
-
 cd devops-notes-app
+git checkout -b test-abc
+git checkout -b test-xyz
+echo "Created new branches"
+
 git checkout test-abc
 touch abc.txt
 git add .
 git commit -m "adding a file"
+git push origin test-abc
 echo "commited to the branch"
 
 git request-pull origin/test-xyz https://github.com/balujaddu/devops-notes-app.git origin/test-abc
