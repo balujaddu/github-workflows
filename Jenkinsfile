0626f688bb70e96b4d4203feb06dc0987f74c206
Balasubrahmanyam <balasubrahmanyam321@gmail.com>
2025-03-12 00:56:29 +0530

    Modified as per the request

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,8 +3,8 @@ pipeline{
     stages{
         stage('github-actions'){
             steps{
-                sh 'chmod +x ./github/test-script-3.sh'
-                sh './github/test-script-3.sh'
+                sh 'chmod +x ./github/script.sh'
+                sh './github/script.sh'
             }
         }
         stage('cleanup-workspace'){
---
+++ github/script.sh
@@ -0,0 +1,29 @@
+git remote set-url origin https://github.com/balujaddu/devops-notes-app.git
+git pull origin master
+echo "Cloning repo completed"
+
+cd devops-notes-app
+git remote -v
+git fetch origin
+git checkout -b test-abc origin/master
+git remote -v
+git fetch origin
+git checkout -b test-xyz origin/test-abc
+echo "Created new branches"
+
+touch xyz.txt
+git add .
+git commit -m "adding a file"
+git push origin test-xyz
+echo "commited to the branch"
+
+git request-pull test-abc https://github.com/balujaddu/devops-notes-app.git test-xyz
+echo "Pull reqiest raised"
+
+git checkout test-abc
+git merge --no-ff origin/test-abc
+echo "PR merged"
+
+cd ..
+rm -rf devops-notes-app
+rm -rf github-worflows
